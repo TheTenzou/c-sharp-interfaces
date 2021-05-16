1257a7e861e2d53e78f30e6186e55af60c2178ee
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-16 12:21:19 +0300

    Initital commit

---
+++ InterfacesTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
